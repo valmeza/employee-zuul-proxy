99ff6491ab59fe1f1cf382092344c5728d217000
Valeria Meza <valeriameza173@gmail.com>
2021-04-02 14:00:20 -0500

    initial setup.

---
+++ src/main/java/com/employeezuulproxy/EmployeeZuulProxyApplication.java
@@ -0,0 +1,13 @@
+package com.employeezuulproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeZuulProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeZuulProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeezuulproxy/EmployeeZuulProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeezuulproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeZuulProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
